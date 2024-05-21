f1003bf2a3fa96b969d3241e423b3472e5368d94
Holeekey <danbrunosv@gmail.com>
2024-05-20 20:18:27 -0400

    test(Root): trainer tests

--- @app/api/src/trainer/application/errors/fetch.followers.failed.ts
+++
@@ -1,8 +0,0 @@
-import { makeApplicationErrorFactory } from 'src/core/application/error/application.error'
-
-export const FETCH_FOLLOWERS_FAILED = 'FETCH_FOLLOWERS_FAILED' as const
-
-export const fetchFollowersFailedError = makeApplicationErrorFactory({
-    name: FETCH_FOLLOWERS_FAILED,
-    message: 'Something went wrong while fetching trainer followers',
-})
--- @app/api/src/trainer/application/errors/fetch.user.follow.failed.ts
+++
@@ -1,8 +0,0 @@
-import { makeApplicationErrorFactory } from 'src/core/application/error/application.error'
-
-export const FETCH_USER_FOLLOW_FAILED = 'FETCH_USER_FOLLOW_FAILED' as const
-
-export const fetchUserFollowFailedError = makeApplicationErrorFactory({
-    name: FETCH_USER_FOLLOW_FAILED,
-    message: 'Something went wrong while fetching user follow',
-})
