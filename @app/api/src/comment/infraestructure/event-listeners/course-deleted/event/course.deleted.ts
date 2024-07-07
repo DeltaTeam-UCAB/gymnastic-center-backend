081e1f58fc2ef15befe9d8374e4d5d76b0d9453c
Alfonsoasadev <alfonsoasadev@gmail.com>
2024-07-07 01:32:42 -0400

    still resolving conflicts with deleted files

--- @app/api/src/comment/infraestructure/event-listeners/course-deleted/event/course.deleted.ts
+++ @app/api/src/comment/infraestructure/event-listeners/course-deleted/event/course.deleted.ts
@@ -3,6 +3,7 @@ import { CourseID } from './value-objects/course.id'
 
 export const COURSE_DELETED = 'COURSE_DELETED'
 
+
 export const courseDeleted = domainEventFactory<{
     id: CourseID
 }>(COURSE_DELETED)
