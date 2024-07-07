902491e7b9480b071e42dcd2c5e400d4d1750358
Daniel Fuentes <titofuentes1000@gmail.com>
2024-07-07 14:58:05 -0400

    fix(Root): api
    Fix some api endpoints

--- @app/api/src/trainer/infraestructure/controllers/find-many/find-many-trainers.controller.ts
+++ @app/api/src/trainer/infraestructure/controllers/find-many/find-many-trainers.controller.ts
@@ -4,7 +4,6 @@ import { Controller } from 'src/core/infraestructure/controllers/decorators/cont
 import { TrainerPostgresRepository } from '../../repositories/postgres/trainer.repository'
 import { ErrorDecorator } from 'src/core/application/decorators/error.handler.decorator'
 import { UserGuard } from 'src/user/infraestructure/guards/user.guard'
-import { Roles, RolesGuard } from 'src/user/infraestructure/guards/roles.guard'
 import { User } from 'src/user/application/models/user'
 import { User as UserDecorator } from 'src/user/infraestructure/decorators/user.decorator'
 import { LoggerDecorator } from 'src/core/application/decorators/logger.decorator'
@@ -20,7 +19,7 @@ import { ImagePostgresByTrainerRepository } from '../../repositories/postgres/im
     bearerAuth: true,
 })
 export class FindTrainerController
-    implements
+implements
         ControllerContract<
             [data: FindManyTrainersDTO, user: User],
             FindManyTrainersResponse[]
@@ -32,8 +31,7 @@ export class FindTrainerController
     ) {}
 
     @Get('many')
-    @Roles('CLIENT')
-    @UseGuards(UserGuard, RolesGuard)
+    @UseGuards(UserGuard)
     async execute(
         @Query() data: FindManyTrainersDTO,
         @UserDecorator() user: User,
