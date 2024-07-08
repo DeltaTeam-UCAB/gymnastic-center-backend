62c3609b06f59a7580d750c71f1c8597ce659826
Daniel Fuentes <titofuentes1000@gmail.com>
2024-07-08 15:34:05 -0400

    fix(Root): trainer
    Fix trainer dependencies

--- @app/api/src/blog/application/commands/create/decorators/trainer.exist.decorator.ts
+++ @app/api/src/blog/application/commands/create/decorators/trainer.exist.decorator.ts
@@ -3,11 +3,11 @@ import { CreateBlogDTO } from '../types/dto'
 import { CreateBlogResponse } from '../types/response'
 import { Result } from 'src/core/application/result-handler/result.handler'
 import { TrainerRepository } from 'src/blog/application/repositories/trainer.repository'
-import { trainerNotFoundError } from 'src/trainer/application/errors/trainer.not.found'
 import { TrainerId } from '../../../../domain/value-objects/trainer.id'
+import { trainerNotFoundError } from 'src/blog/application/errors/trainer.not.found'
 
 export class TrainerExistDecorator
-    implements ApplicationService<CreateBlogDTO, CreateBlogResponse>
+implements ApplicationService<CreateBlogDTO, CreateBlogResponse>
 {
     constructor(
         private service: ApplicationService<CreateBlogDTO, CreateBlogResponse>,
